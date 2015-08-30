cad8ac843c7ccaa0428d8c97bff35e79a3e25a44
i042416 <jerry.wang@sap.com>
2015-08-30 13:12:00 +0800

    2+2 = 5

---
+++ src/two.java
@@ -0,0 +1,12 @@
+import java.lang.reflect.Field;
+
+public class two {
+	public static void main(String[] args) throws Exception {
+		Class cache = Integer.class.getDeclaredClasses()[0];
+		Field c = cache.getDeclaredField("cache");
+		c.setAccessible(true);
+		Integer[] array = (Integer[]) c.get(cache);
+		array[132] = array[133];
+		System.out.printf("%d",2 + 2);
+	}
+}
