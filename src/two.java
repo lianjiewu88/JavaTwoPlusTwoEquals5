326823d1e9f4d0cadb462b44d22811e7538d727e
i042416 <jerry.wang@sap.com>
2015-08-30 13:29:29 +0800

    add ignore .gitignore

--- src/two.java
+++ src/two.java
@@ -1,7 +1,16 @@
 import java.lang.reflect.Field;
 
 public class two {
+	
+	// touch .gitignore
+	public static void getCurrentName() {
+		String methodName = Thread.currentThread().getStackTrace()[1].getMethodName();
+		System.out.println(methodName);
+		
+	}
+	
 	public static void main(String[] args) throws Exception {
+		getCurrentName();
 		Class cache = Integer.class.getDeclaredClasses()[0];
 		Field c = cache.getDeclaredField("cache");
 		c.setAccessible(true);
