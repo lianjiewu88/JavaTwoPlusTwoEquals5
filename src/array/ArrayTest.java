818f30b3137796808e8daf5e57f9a594cbae7694
i042416 <jerry.wang@sap.com>
2016-01-15 21:21:34 +0800

    constructor test

---
+++ src/array/ArrayTest.java
@@ -0,0 +1,48 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayTest {
+
+	private static void asListTest(){
+		String[] a = {"A", "B"};
+		List<String> list = Arrays.asList(a);
+		System.out.println(list.getClass().getName()); // java.util.Arrays$ArrayList
+		System.out.println(list.contains("A")); // true
+		list.add("C");
+		/*
+		 * Jerry: 2016-01-15 9:09PM:
+		 * Exception in thread "main" java.lang.UnsupportedOperationException
+	at java.util.AbstractList.add(Unknown Source)
+	at java.util.AbstractList.add(Unknown Source) 
+	Arrays.asList() 会返回一个ArrayList，这是Arrays里内嵌的一个私有静态类，而并不是java.util.ArrayList类java.util.Arrays.ArrayList 有set(), get(), contains()方法，但并支持添加元素，所以大小是固定的，想要创建一个真正的ArrayList，你应该：
+ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList(arr));
+		 */
+	}
+	
+	private static void removeInLoop() {
+		ArrayList<String> list = new ArrayList<String>(Arrays.asList("a", "b", "c", "d"));
+
+		/* ConcurrentModificationException
+		for (String s : list) {
+		    if (s.equals("a"))
+		        list.remove(s);
+		}*/
+		Iterator<String> iter = list.iterator();
+		while (iter.hasNext()) {
+		    String s = iter.next();
+
+		    if (s.equals("a")) {
+		        iter.remove();
+		    }
+		}
+		list.forEach(System.out::println);
+	}
+	public static void main(String[] args) {
+		// asListTest();
+		removeInLoop();
+	}
+}
---
+++ src/constructor/ConstructorTest.java
@@ -0,0 +1,38 @@
+package constructor;
+
+class Super {
+    String s;
+    public Super(String s){
+       this.s = s;
+    }
+}
+/*
+ * Jerry 2016-01-15 21:17PM
+ * 编译出错的原因主要是默认的Super类的构造函数并没有被定义。在Java中，如果一个类没有实现构造函数，编译器默认会给这个类插入一个无参构造函数。如果在父类中已经有了构造函数，那么编译器将不会再插入默认无参构造函数。
+子类的构造函数，要么是带参的要么是无参的，都会调用父类的无参构造。因为编译器想在子类中，加入super()，但是父类的无参构造函数并不存在。所以，编译器会报错。
+为了解决这个问题，有两种解决办法，第一，在Super类中，加入无参构造：
+public Super(){
+    System.out.println("Super");
+}
+第二种方式，移除自定义的父类构造函数
+第三种方式，子类的构造函数中加入super(value)
+ */
+
+class Sub extends Super {
+     public Sub(String s ) {
+    	 super(s);
+     }
+     public Sub(){
+    	 super("dummy");
+     }
+}
+
+
+public class ConstructorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
