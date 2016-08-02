b09a196ae3631562a3861f9907ff418d4b47549d
cle.ee@163.com <cle.ee@163.com>
2016-08-02 11:24:04 +0800

    static test

--- src/basic/FloatTest.java
+++ src/basic/FloatTest.java
@@ -1,8 +1,10 @@
 package basic;
 
+import java.lang.reflect.Field;
 import java.math.BigDecimal;
 
 public class FloatTest {
+	public static String A = "JerryTest";
 	public static void main(String[] args) {
 	 float a = (float) 1.03;
 	 float b = (float) .42;
@@ -20,5 +22,24 @@ public class FloatTest {
 	 BigDecimal b1 = new BigDecimal(".42");
 	 System.out.println(a1.multiply(b1));
 	 System.out.println(a1.subtract(b1));
+	 
+	 FloatTest tool = new FloatTest();
+	 System.out.println(tool.A);
+	 Object classObject = FloatTest.class;
+	 
+	 Class<FloatTest> convert = (Class<FloatTest>)classObject;
+	 Field[] fields = convert.getFields();
+	 Object classObject2 = FloatTest.class.getClass();
+	 Field staticField = fields[0];
+	 try {
+		System.out.println("static field value: " + staticField.get(tool));
+	} catch (IllegalArgumentException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IllegalAccessException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	 
 	}
 }
