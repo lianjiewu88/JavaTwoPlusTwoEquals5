ba1d5f146cdef3df7bab22b14859072b1e5ce11a
cle.ee@163.com <cle.ee@163.com>
2016-08-07 09:39:15 +0800

    thread volatile test

---
+++ src/thread/ThreadVerify.java
@@ -0,0 +1,23 @@
+package thread;
+
+public class ThreadVerify {
+	 public volatile static boolean  stop = false;
+	 public static void main(String args[]) throws InterruptedException {
+	     Thread testThread = new Thread(){
+	           @Override
+	           public void run(){
+	             int i = 1;
+	             while(!stop){
+	            	 //System.out.println("in thread: " + Thread.currentThread() + " i: " + i);
+	                 i++;
+	             }
+	             System.out.println("Thread stop i="+ i);
+	          }
+	        };
+	        testThread.start();
+	        Thread.sleep(1000);
+	        stop = true;
+	        System.out.println("now, in main thread stop is: " + stop);
+	        testThread.join();
+	    }
+	}
