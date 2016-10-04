e0ec3b808c64b61d5d80850c07120899c0f617ae
cle.ee@163.com <cle.ee@163.com>
2016-10-04 22:59:04 +0800

    converted javap

--- src/constant/ConstantFolding.java
+++ src/constant/ConstantFolding.java
@@ -1,19 +1,24 @@
 package constant;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConstantFolding {
 
-	static final  int number1 = 5;
+	/*static final  int number1 = 512;
 	 
-    static final  int number2 = 6;
+    static final  int number2 = 623;
  
-    static int number3 = 5;
+    static int number3 = 545;
  
-    static int number4= 6;
+    static int number4= 619;*/
 
 	public static void main(String[] args) {
-		 int product1 = number1 * number2;         //line A
-         int product2 = number3 * number4;         //line B
-
+		 /*int product1 = number1 * number2;         
+         int product2 = number3 * number4;         
+         System.out.println("Value: " + product1 + " , " + product2);*/
+		// converted: List myList = new ArrayList(10);
+		List<String> myList = new ArrayList<String>(10);
 	}
 
 }
