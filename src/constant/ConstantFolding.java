7ed4e267708f1392bfc7cb618336113f5aae638a
cle.ee@163.com <cle.ee@163.com>
2016-10-05 09:30:12 +0800

    javap

--- src/constant/ConstantFolding.java
+++ src/constant/ConstantFolding.java
@@ -18,6 +18,14 @@ public class ConstantFolding {
          int product2 = number3 * number4;         
          System.out.println("Value: " + product1 + " , " + product2);*/
 		// converted: List myList = new ArrayList(10);
+		/*
+		 * 泛型（又称类型检验）：这个是发生在编译期的。
+		 * 编译器负责检查程序中类型的正确性，然后把使用了泛型的代码翻译或者
+		 * 重写成可以执行在当前JVM上的非泛型代码。这个技术被称为“类型擦除“。
+		 * 换句话来说，编译器会擦除所有在尖括号里的类型信息，来保证和版本1.4.0或者
+		 * 更早版本的JRE的兼容性。
+		 */
+		
 		List<String> myList = new ArrayList<String>(10);
 	}
 
