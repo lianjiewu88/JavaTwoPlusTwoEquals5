2f4455a984114510d2c00feaf6cd90d672365367
cle.ee@163.com <cle.ee@163.com>
2017-01-14 10:03:04 +0800

    add CallableTest, Jerry's own adaptation

--- src/callableTest/CallableTest.java
+++ src/callableTest/CallableTest.java
@@ -8,39 +8,62 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+class CallableIndicator{
+	private String name;
+	private Double result;
+	
+	public CallableIndicator(String name, Double result){
+		this.name = name;
+		this.result = result;
+	}
+	
+	public Double getResult(){
+		return this.result;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+}
+
 public class CallableTest {
 	 private static final int POOL_SIZE = 10;
 	 
-	    static class CalcThread implements Callable<Double> {
+	    static class CalcThread implements Callable<CallableIndicator> {
+	    	
+	    	private String name;
 	        private List<Double> dataList = new ArrayList<>();
 	 
-	        public CalcThread() {
+	        public CalcThread(String name) {
 	            for(int i = 0; i < 10000; ++i) {
 	                dataList.add(Math.random());
 	            }
+	            this.name = name;
 	        }
 	 
 	        @Override
-	        public Double call() throws Exception {
+	        public CallableIndicator call() throws Exception {
+	        	
 	            double total = 0;
 	            for(Double d : dataList) {
 	                total += d;
 	            }
-	            return total / dataList.size();
+	            return new CallableIndicator(this.name, total / dataList.size());
 	        }
 	 
 	    }
 	 
 	    public static void main(String[] args) {
-	        List<Future<Double>> fList = new ArrayList<>();
+	        List<Future<CallableIndicator>> fList = new ArrayList<>();
 	        ExecutorService es = Executors.newFixedThreadPool(POOL_SIZE);
 	        for(int i = 0; i < POOL_SIZE; ++i) {
-	            fList.add(es.submit(new CalcThread()));
+	            fList.add(es.submit(new CalcThread("Jerry Thread: " + i)));
 	        }
 	 
-	        for(Future<Double> f : fList) {
+	        for(Future<CallableIndicator> f : fList) {
 	            try {
-	                System.out.println(f.get());
+	                System.out.println("Thread name: " + f.get().getName() + " result: " +  f.get().getResult());
+	                
 	            } catch (Exception e) {
 	                e.printStackTrace();
 	            }
