2a2268a15beb71f1eab98141f8d108d9480c3de4
cle.ee@163.com <cle.ee@163.com>
2017-01-18 18:26:19 +0800

    volatile example 1000

---
+++ src/volatileTest/VolatileTest01.java
@@ -0,0 +1,31 @@
+package volatileTest;
+
+public class VolatileTest01 {
+    volatile int i;
+
+    // synchronized if commented out, sum will not equal to 1000
+    public void  addI(){
+        i++;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final  VolatileTest01 test01 = new VolatileTest01();
+        for (int n = 0; n < 1000; n++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    test01.addI();
+                }
+            }).start();
+        }
+
+        Thread.sleep(10000);//等待10秒，保证上面程序执行完成
+
+        System.out.println(test01.i);
+    }
+}
