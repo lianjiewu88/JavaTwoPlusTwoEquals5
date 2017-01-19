72c4751629a641ddfa868c0f42a26184589bef3f
cle.ee@163.com <cle.ee@163.com>
2017-01-19 17:04:33 +0800

    verified, discussed with Ben

--- src/interfaceTest/InterfaceTest.java
+++ src/interfaceTest/InterfaceTest.java
@@ -14,12 +14,57 @@ class CL_Java implements IF_Java{
 		return 1;
 	}
 }
+
+class PureTest{
+	private String name;
+	public PureTest(String name){
+		this.name = name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void say(){
+		System.out.println("I am: " + this.name);
+	}
+}
+
+
 public class InterfaceTest {
 
+	private static void hello1(PureTest test){
+		test.say();
+		test = null;
+	}
+	
+	private static void hello2(PureTest test){
+		test.setName("Tom");
+	}
+	
+	private static void hello3(PureTest test){
+		test = null;
+		test = new PureTest("Jay");
+	}
 	public static void main(String[] args) {
 		IF_Java result = CL_Java.CreateVersionAPI();
 		System.out.println(result.getVersion());
-
+		
+		PureTest test1 = new PureTest("Jerry");
+		test1.say();
+		hello1(test1);
+		test1.say();
+		/*
+		 * I am: Jerry
+			I am: Jerry
+			I am: Jerry
+		 */
+		
+		hello2(test1);
+		test1.say(); // I am: Tom
+		
+		hello3(test1);
+		test1.say(); // I am: Tom
 	}
 
 }
