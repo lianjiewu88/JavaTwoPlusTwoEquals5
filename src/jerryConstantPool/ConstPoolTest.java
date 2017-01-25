ac350d0df544c74c2c4891e371e16a8df860b85c
cle.ee@163.com <cle.ee@163.com>
2017-01-25 16:45:57 +0800

    constant pool add to github issue

---
+++ src/jerryConstantPool/ConstPoolTest.java
@@ -0,0 +1,10 @@
+package jerryConstantPool;
+
+public class ConstPoolTest {
+
+	public static void main(String[] args) {
+	        String str1 = "Jerry concurrentHashMap";
+	        String str2 = new String("Jerry concurrentHashMap");
+	        System.out.printf("str1==str2 is %b",str1==str2);
+	}
+}
