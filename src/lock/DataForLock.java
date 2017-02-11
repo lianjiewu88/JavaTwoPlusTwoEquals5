913205eec91f6a7a82bd0cd8f09463128b80adb2
cle.ee@163.com <cle.ee@163.com>
2017-02-11 12:04:26 +0800

    add class DataForLock

---
+++ src/lock/DataForLock.java
@@ -0,0 +1,37 @@
+package lock;
+
+public class DataForLock {
+	 
+    private final char[] buffer;
+ 
+    public DataForLock(int size) {
+        this.buffer = new char[size];
+        for (int i = 0; i < size; i++) {
+            buffer[i] = '*';
+        }
+    }
+ 
+    public String read() {
+        StringBuilder result = new StringBuilder();
+        for (char c : buffer) {
+            result.append(c);
+        }
+        sleep(100);
+        return result.toString();
+    }
+ 
+    public void write(char c) {
+        for (int i = 0; i < buffer.length; i++) {
+            buffer[i] = c;
+            sleep(100);
+        }
+    }
+ 
+    private void sleep(long ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
