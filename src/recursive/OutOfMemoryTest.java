725f161580e31e2761c15c237502ba7dcdb197a3
cle.ee@163.com <cle.ee@163.com>
2017-02-19 15:04:55 +0800

    strange, my own recuisive call will not generate heap dump file, why?

--- src/recursive/OutOfMemoryTest.java
+++ src/recursive/OutOfMemoryTest.java
@@ -5,12 +5,16 @@ public class OutOfMemoryTest {
 	public static void calc(int arg1, int arg2){
 		int _arg1 = arg1;
 		int _arg2 = arg2;
-		System.out.println(arg1);
+		//System.out.println(arg1);
 		calc( arg1 + arg2 , arg2);
 		
 	}
 	public static void main(String[] args) {
-		calc(1,2);
+		long arr[];      
+	    for (int i=1; i<=10000000; i*=2) {  
+	        arr = new long[i];  
+	    }  
+		// calc(1,2);
 	}
 
 }
