ac5397e5ddf242128b2d4ee77de290fc9f21e27a
cle.ee@163.com <cle.ee@163.com>
2017-02-19 15:08:10 +0800

    add detail comment for later check

--- src/recursive/OutOfMemoryTest.java
+++ src/recursive/OutOfMemoryTest.java
@@ -8,10 +8,12 @@ public class OutOfMemoryTest {
 		int _arg1 = arg1;
 		int _arg2 = arg2;
 		//System.out.println(arg1);
+		// Jerry 2017-02-19 3:07: this operation of mine could NOT generate heap dump file
 		calc( arg1 + arg2 , arg2);
 		
 	}
 	public static void main(String[] args) {
+		// this operation can however generate heap file as expected, strange
 		long arr[];      
 	    for (int i=1; i<=10000000; i*=2) {  
 	        arr = new long[i];  
