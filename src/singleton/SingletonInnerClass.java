3b8ef778481e3f0ee0ac17a7bab8b8dee0bae428
cle.ee@163.com <cle.ee@163.com>
2017-02-20 10:54:26 +0800

    static inner class to implement singleton

---
+++ src/singleton/SingletonInnerClass.java
@@ -0,0 +1,29 @@
+package singleton;
+
+public class SingletonInnerClass {
+	
+	/*
+	 * 当执行getInstance()方法的时候就去调用FileIOHolder内部类里面的INSTANCE实例，
+	 * 此时FileIOHolder内部类会被加载到内存里，在类加载的时候就对INSTANCE实例进行初始化。
+	 * 和饿汉式一个道理，保证了只有一个实例，而且在调用getInstance()方法的时候才进行INSTANCE实例的初始化，
+	 * 又具有懒汉式的部分特性。”
+	 */
+	private static final class FileIOHolder {
+		private static final SingletonInnerClass INSTANCE = new SingletonInnerClass();
+	}
+
+	private SingletonInnerClass() {
+		System.out.println("constructor of SingletonInnerClass...");
+	}
+
+	public static SingletonInnerClass getInstance() {
+		System.out.println("GetInstance called...");
+		return FileIOHolder.INSTANCE;
+	}
+	
+	public static void main(String arg[]){
+		Object obj1 = SingletonInnerClass.getInstance();
+		Object obj2 = SingletonInnerClass.getInstance();
+		System.out.println(obj1 == obj2);
+	}
+}
