35e1702d7501026e6081b152961bdc2da4e36ff9
cle.ee@163.com <cle.ee@163.com>
2017-03-04 18:42:44 +0800

    executor test and automatic integer

--- src/thread/Executor.java
+++ src/thread/Executor.java
@@ -1,17 +1,31 @@
 package thread;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
 class JerryThread implements Runnable{
 
+	private List<Integer> myList;
+	private AtomicInteger myCounter;
+	public JerryThread(List<Integer> list, AtomicInteger counter){
+		this.myList = list;
+		this.myCounter = counter;
+	}
 	@Override
 	public void run() {
-		 while(true){
-             System.out.println("begincode");
+		int newValue = 0;
+		 while(newValue < 100){
+             
+			 newValue = myCounter.incrementAndGet();
+             System.out.println("begincode: " + Thread.currentThread().getName() + " new value: " 
+            		 + newValue);
+             myList.add(newValue);
              try {
-                 Thread.sleep(1000);
+                 Thread.sleep(50);
+                 
              } catch (InterruptedException e) {
                  e.printStackTrace();
              }
@@ -22,16 +36,25 @@ class JerryThread implements Runnable{
 public class Executor {
 
 	private AtomicInteger atomicI = new AtomicInteger(0);
+	private ArrayList<Integer> taskList = new ArrayList<Integer>();
 	
-	private static void launch(){
+	private void launch(){
 		
-		//定义了线程池中最大存在的线程数目
         ExecutorService executorService= Executors.newFixedThreadPool(10);
-        //添加了一个任务...
-        executorService.execute(new JerryThread());
+
+        executorService.execute(new JerryThread(taskList, atomicI));
+        executorService.execute(new JerryThread(taskList, atomicI));
+        executorService.execute(new JerryThread(taskList, atomicI));
+        executorService.execute(new JerryThread(taskList, atomicI));
+        executorService.execute(new JerryThread(taskList, atomicI));
+        executorService.execute(new JerryThread(taskList, atomicI));
+        executorService.execute(new JerryThread(taskList, atomicI));
+        executorService.execute(new JerryThread(taskList, atomicI));
+        executorService.execute(new JerryThread(taskList, atomicI));
 	}
 	public static void main(String[] args) {
-		launch();
+		Executor test = new Executor();
+		test.launch();
 	}
 
 }
