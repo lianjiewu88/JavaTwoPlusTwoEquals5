9d3a5258511920d535f10aa63b8e86d000fd2ea0
cle.ee@163.com <cle.ee@163.com>
2017-03-04 21:12:43 +0800

    finish blog: ArrayList in multi-thread context

--- src/thread/MyExecutor.java
+++ src/thread/MyExecutor.java
@@ -1,10 +1,12 @@
 package thread;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Vector;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
 
 class MyThread implements Runnable{
 
@@ -17,37 +19,48 @@ class MyThread implements Runnable{
 		this.host = object;
 	}
 	
-	public void updateList(int i){
+	public void updateListSafe(){
+		synchronized(this.host){
+			ArrayList<Integer> safe = new ArrayList<Integer>();
+			safe.add(1);
+		}
+	}
+	
+	private void updateList(int i){
 		synchronized(this.host){
 			myList.add(i);
 		}
 	}
 	
-	public void updateVector(int i){
+	public void updateListSafe(int i){
+			myList.add(i);
+	}
+	private void updateVector(int i){
 		myVector.add(i);
 	}
 	
 	@Override
 	public void run() {
 		for( int i = 0; i < 10;i++){
-			// updateList(i);
-			updateVector(i);
+			updateListSafe(i);
+			// updateVector(i);
 		}
-		System.out.println("end: " + myVector.size());
+		System.out.println("end: " + myList.size());
 	}
 	
 }
 public class MyExecutor {
 
 	private ArrayList<Integer> taskList = new ArrayList<Integer>();
+	private List<Integer> safe = Collections.synchronizedList(taskList);
 	private Vector<Integer> vector = new Vector<Integer>();
 	private Object object = new Object();
 	private void launch(){
 		
         ExecutorService executorService= Executors.newFixedThreadPool(10);
 
-        executorService.execute(new MyThread(taskList, vector, object));
-        executorService.execute(new MyThread(taskList, vector, object));
+        executorService.execute(new MyThread(safe, vector, object));
+        executorService.execute(new MyThread(safe, vector, object));
 	}
 	public static void main(String[] args) {
 		MyExecutor test = new MyExecutor();
