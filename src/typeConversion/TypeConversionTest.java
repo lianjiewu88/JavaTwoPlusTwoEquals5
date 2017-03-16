d354f8f070b3aecc660ab8eb3fe8f23a5060ca19
cle.ee@163.com <cle.ee@163.com>
2017-03-16 19:00:09 +0800

    a stupid runtime type check

--- src/typeConversion/TypeConversionTest.java
+++ src/typeConversion/TypeConversionTest.java
@@ -1,5 +1,8 @@
 package typeConversion;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class TypeConversionTest {
 
 	private static void test( long a){
@@ -10,8 +13,16 @@ public class TypeConversionTest {
 		long b = a;
 		double c = 1;
 		// Type mismatch: cannot convert from double to int
-		int d = c;
+		// int d = c;
 		test(a);
+		
+		RunTimeCheck(new HashMap<Integer,String>());
+	}
+	
+	public static void RunTimeCheck(Object object){
+		ArrayList<String> temp = new ArrayList<String>();
+		
+		temp = (ArrayList<String>) object;
 	}
 
 }
