f5ccef6c0fabadb4dd5c4de4cc787d9528c9f719
cle.ee@163.com <cle.ee@163.com>
2017-03-16 13:59:25 +0800

    create first annotation

---
+++ src/AnnotationTest/Description.java
@@ -0,0 +1,15 @@
+package AnnotationTest;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD,ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+public @interface Description {
+	 String desc();
+	 String author();
+	 int age() default 18;
+}
