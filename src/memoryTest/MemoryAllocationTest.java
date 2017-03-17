4fdcdb1d92fdc28aad71379b128b65ae72c8e865
cle.ee@163.com <cle.ee@163.com>
2017-03-17 21:18:59 +0800

    allocate 1MB

--- src/memoryTest/MemoryAllocationTest.java
+++ src/memoryTest/MemoryAllocationTest.java
@@ -5,18 +5,16 @@ import java.util.TimerTask;
 
 class MyTask extends TimerTask{
 
+	static final int MB = 1000 * 1024 * 1024;
 	@Override
 	public void run() {
 		 Runtime runtime = Runtime.getRuntime();
-         System.out.print("total:"+(runtime.totalMemory()/1024)+ "k\n");
-         long free=runtime.freeMemory()/1024;
-         System.out.print("free:" + free+ "k\n");
-         if(free<102400){
-             System.out.print("need gc"+"\n");
-         }
-         byte[] a1 = new byte[100 * 1024 * 1024];
+         System.out.print("total :" + (runtime.totalMemory() / 1024)+ " KB\n");
+         long free = runtime.freeMemory() / 1024;
+         System.out.print("free:" + free+ " KB\n");
+
+         byte[] a1 = new byte[1 * MB];
          a1[1] = 1;
-         System.out.print(a1[1]+"\n");
 	}
 }
 
