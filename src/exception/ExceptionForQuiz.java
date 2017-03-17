aa662f44a7c6f54d510555d85d753daa3a72fbb7
cle.ee@163.com <cle.ee@163.com>
2017-03-17 18:21:24 +0800

    blog done

---
+++ src/exception/ExceptionForQuiz.java
@@ -0,0 +1,19 @@
+package exception;
+
+import java.sql.SQLException;
+
+public class ExceptionForQuiz<T extends Exception> {
+	private void pleaseThrow(final Exception t) throws T {
+		throw (T) t;
+	}
+
+	public static void main(final String[] args) {
+		try {
+			new ExceptionForQuiz<RuntimeException>()
+					.pleaseThrow(new SQLException());
+		} catch (final Exception ex) {
+			System.out.println("Jerry print, the exception class: " + ex.getClass().getSimpleName());			
+			ex.printStackTrace();
+		}
+	}
+}
