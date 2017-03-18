3ffad75d6e5ff2983c714924d84654d13360e073
cle.ee@163.com <cle.ee@163.com>
2017-03-18 10:25:44 +0800

    blog for Java part done

--- src/memoryTest/MemoryAllocationTest.java
+++ src/memoryTest/MemoryAllocationTest.java
@@ -5,16 +5,15 @@ import java.util.TimerTask;
 
 class MyTask extends TimerTask{
 
-	static final int MB = 1000 * 1024 * 1024;
+	static final int GB = 1000 * 1024 * 1024;
 	@Override
 	public void run() {
+		 byte[] a1 = new byte[1 * GB];
+         a1[1] = 1;
 		 Runtime runtime = Runtime.getRuntime();
          System.out.print("total :" + (runtime.totalMemory() / 1024)+ " KB\n");
          long free = runtime.freeMemory() / 1024;
          System.out.print("free:" + free+ " KB\n");
-
-         byte[] a1 = new byte[1 * MB];
-         a1[1] = 1;
 	}
 }
 
