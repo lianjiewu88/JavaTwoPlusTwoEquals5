31e7a85432620fc4904a83db83421a99f057e39f
cle.ee@163.com <cle.ee@163.com>
2017-04-10 17:01:29 +0800

    begin another blog

--- src/innerTest/LocalClassConsumer.java
+++ src/innerTest/LocalClassConsumer.java
@@ -3,5 +3,6 @@ package innerTest;
 public class LocalClassConsumer {
 	public static void main(String[] arg){
 		System.out.println(new LocalClassTest().add(1, 2));
+	    new lcl_test();
 	}
 }
--- src/integerTest/IntegerTest.java
+++ src/integerTest/IntegerTest.java
@@ -1,16 +1,19 @@
 package integerTest;
 
 import java.lang.reflect.Field;
+
 public class IntegerTest {
 
-	    public static void main(String[] args) throws Exception {
-	        Class cache = Integer.class.getDeclaredClasses()[0];
-	        Field c = cache.getDeclaredField("cache");
-	        c.setAccessible(true);
-	        Integer[] array = (Integer[]) c.get(cache);
-	        array[132] = array[133];
-	 
-	        System.out.printf("%d",2 + 2);
-	    }
+	private static void doSomethingMagic() throws Exception {
+		Class cache = Integer.class.getDeclaredClasses()[0];
+		Field c = cache.getDeclaredField("cache");
+		c.setAccessible(true);
+		Integer[] array = (Integer[]) c.get(cache);
+		array[132] = array[133];
 	}
 
+	public static void main(String[] args) throws Exception {
+		doSomethingMagic();
+		System.out.printf("2 + 2 = %d", 2 + 2);
+	}
+}
