6fd1b2d64894e75e74832af5d1ca38590489e77b
cle.ee@163.com <cle.ee@163.com>
2017-04-17 08:26:53 +0800

    start proxy pattern

---
+++ src/proxy/DeveloperTest.java
@@ -0,0 +1,9 @@
+package proxy;
+
+public class DeveloperTest {
+
+	public static void main(String[] args) {
+		Developer jerry = new Developer("Jerry");
+		jerry.writeCode();
+	}
+}
---
+++ src/proxy/IDeveloper.java
@@ -0,0 +1,5 @@
+package proxy;
+
+public interface IDeveloper {
+	public void writeCode();
+}
