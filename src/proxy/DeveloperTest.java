3b1a2b5c02fcda7aa14a8bf36cb732ae3cbcf0dd
cle.ee@163.com <cle.ee@163.com>
2017-04-17 10:03:02 +0800

    dynamic proxy handler

--- src/proxy/DeveloperTest.java
+++ src/proxy/DeveloperTest.java
@@ -4,8 +4,13 @@ public class DeveloperTest {
 
 	public static void main(String[] args) {
 		
+		IDeveloper jerry = new Developer("Jerry");
 		ITester sara = new Tester("Sara");
-		ITester saraProxy = new TesterProxy(sara);
+		
+		IDeveloper jerryProxy = (IDeveloper) new EnginnerProxy().bind(jerry);
+		jerryProxy.writeCode();
+		
+		ITester saraProxy = (ITester) new EnginnerProxy().bind(sara);
 		saraProxy.doTesting();
 	}
 }
