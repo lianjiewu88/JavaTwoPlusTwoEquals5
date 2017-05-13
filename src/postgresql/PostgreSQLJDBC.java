d6d6b1dc46be475b8494c14bb5e70135fb57ee85
cle.ee@163.com <cle.ee@163.com>
2017-05-13 09:06:25 +0200

    postgreSQL getString

--- src/postgresql/PostgreSQLJDBC.java
+++ src/postgresql/PostgreSQLJDBC.java
@@ -1,7 +1,9 @@
 package postgresql;
 
+import java.sql.Clob;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -42,6 +44,48 @@ public class PostgreSQLJDBC {
 
 	public static void main(String args[]) {
 		PostgreSQLJDBC jdbcTest = new PostgreSQLJDBC();
-		jdbcTest.select();
+		//jdbcTest.select();
+		jdbcTest.clobTest();
+	}
+	
+	private void clobTest(){
+		
+		String description = null;
+	    Clob myClob = null;
+	    PreparedStatement pstmt = null;
+
+	    try {
+	    	connection = DriverManager.getConnection(
+					"jdbc:postgresql://localhost:9812/zproduct", "postgres", "Saptest1");
+	        String sql =
+	            "select text " +
+	            "from public.ztest2 " +
+	            "where key1 = ?";
+
+	        pstmt = this.connection.prepareStatement(sql);
+	        pstmt.setString(1, "1");
+	        ResultSet rs = pstmt.executeQuery();
+
+	        if (rs.next()) {
+	        	System.out.println(rs.getString(1));
+	            //myClob = rs.getClob(1);
+	            //System.out.println("Length of retrieved Clob: " +
+	            //    myClob.length());
+	        }
+	        // description = myClob.getSubString(1, 10);
+	    } catch (SQLException sqlex) {
+	        sqlex.printStackTrace();
+	    } catch (Exception ex) {
+	        System.out.println("Unexpected exception: " + ex.toString());
+	    } finally {
+	        if (pstmt != null)
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	    }
+	    // System.out.println("Description: " + description);
 	}
 }
