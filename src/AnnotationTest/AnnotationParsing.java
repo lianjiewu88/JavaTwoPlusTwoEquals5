bd7d55069b465d2e2d68b38709147b7b664ddcc0
cle.ee@163.com <cle.ee@163.com>
2017-05-19 09:14:17 +0200

    purify annotation example

--- src/AnnotationTest/AnnotationParsing.java
+++ src/AnnotationTest/AnnotationParsing.java
@@ -15,21 +15,13 @@ public class AnnotationParsing {
                     System.out.println(annotation + " in method:"+ method);
                 }
 
+                System.out.println("\r\n");
                 MethodInfo methodInfo = method.getAnnotation(MethodInfo.class);
 
                 if ("1.0".equals(methodInfo.version())) {
                     System.out.println("Method with revision no 1.0 = "
                             + method);
                 }
-                /* output: 
-                 * @AnnotationTest.MethodInfo(version=1.0, author=xxx, date=2015/03/26, comment=override toString()) in method:public java.lang.String AnnotationTest.AnnotationExample.toString()
-Method with revision no 1.0 = public java.lang.String AnnotationTest.AnnotationExample.toString()
-@AnnotationTest.MethodInfo(version=1.0, author=Pankaj, comment=Main method, date=Nov 17 2012) in method:public static void AnnotationTest.AnnotationExample.genericsTest()
-Method with revision no 1.0 = public static void AnnotationTest.AnnotationExample.genericsTest()
-@java.lang.Deprecated() in method:public static void AnnotationTest.AnnotationExample.oldMethod()
-@AnnotationTest.MethodInfo(version=1.0, author=hupeng, comment=deprecated method, date=2015/03/26) in method:public static void AnnotationTest.AnnotationExample.oldMethod()
-Method with revision no 1.0 = public static void AnnotationTest.AnnotationExample.oldMethod()
-                 */
             }
         }
     }
--- src/AnnotationTest/MethodInfo.java
+++ src/AnnotationTest/MethodInfo.java
@@ -1,7 +1,6 @@
 package AnnotationTest;
 
 import java.lang.annotation.*;
-import java.lang.reflect.Method;
 
 @Documented
 @Target(ElementType.METHOD)
