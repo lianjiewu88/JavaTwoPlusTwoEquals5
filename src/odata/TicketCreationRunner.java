9ef154413fb11a06dca69cc533af8f2aa0cbef45
cle.ee@163.com <cle.ee@163.com>
2017-08-25 17:36:03 +0800

    request could reached to server, however 400 error

--- src/odata/TicketCreationRunner.java
+++ src/odata/TicketCreationRunner.java
@@ -1,20 +1,15 @@
 package odata;
 
-
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
 
+import odata.model.IssuePriority;
 import odata.model.Ticket;
 
 import org.apache.http.Header;
@@ -29,17 +24,21 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.olingo.odata2.api.batch.BatchException;
 import org.apache.olingo.odata2.api.client.batch.BatchChangeSet;
 import org.apache.olingo.odata2.api.client.batch.BatchChangeSetPart;
 import org.apache.olingo.odata2.api.client.batch.BatchPart;
 import org.apache.olingo.odata2.api.client.batch.BatchSingleResponse;
 import org.apache.olingo.odata2.api.ep.EntityProvider;
+import org.apache.olingo.odata2.api.ep.EntityProviderException;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class TicketCreationRunner implements Runnable{
 
-	private final String SERVICEURL = "https://qxl-cust233.dev.sapbydesign.com/sap/c4c/odata/v1/c4codata/AccountCollection";
+	private final String SERVICEURL = "https://qxl-cust233.dev.sapbydesign.com/sap/c4c/odata/v1/c4codata/ServiceRequestCollection";
 	private HttpClient m_httpClient;
 	private RequestConfig mConfig;
 	private String boundary = "batch_" + UUID.randomUUID().toString();
@@ -84,17 +83,52 @@ public class TicketCreationRunner implements Runnable{
 		HttpEntity entity = new StringEntity(body);
 
 		post.setEntity(entity);
+		post.setConfig(mConfig);
 
-		for (Header h : post.getAllHeaders()) {
-			logger.info(h.getName() + " : " + h.getValue());
-		}
 
 		HttpResponse response = getHttpClient().execute(post);
 
-		logger.info("Response statusCode => "
+		System.out.println("Response statusCode for Batch => "
 				+ response.getStatusLine().getStatusCode());
 
 		return response;
+	}
+	
+	public String serializeTicketDeepInsert(Ticket t)
+			throws EntityProviderException, IOException, Exception {
+
+		// Input fields Priority, ProductID, IssueCategory, Name & customer
+		// note
+		Map<String, Object> prop = new HashMap<String, Object>();
+
+		if (t.getIssuePriority() != null) {
+			prop.put("ServicePriorityCode", t.getIssuePriority().getPriorityCode());
+		}
+
+		if (!StringUtils.isBlank(t.getProductId())) {
+			prop.put("ProductID", t.getProductId());
+		}
+
+		if (!StringUtils.isBlank(t.getName())) {
+			prop.put("Name", t.getName());
+		}
+
+		if (!StringUtils.isBlank(t.getIssueCategory())) {
+			prop.put("ServiceIssueCategoryID", t.getIssueCategory());
+		}
+		
+		Map<String, Object> propNotes = new HashMap<String, Object>();
+		/*if (t.getNotes() != null) {
+			propNotes.put("Text", t.getNotes().get(0).getNoteDescription());
+			propNotes.put("TypeCode", t.getNotes().get(0).getNoteType().getTypeCode()); // Incident description
+			ArrayList<Map<String, Object>> arrayMap = new ArrayList<Map<String, Object>>();
+			arrayMap.add(propNotes);
+			prop.put("ServiceRequestDescription", arrayMap);
+		}*/
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		return mapper.writeValueAsString(prop);
 	}
 	
 	@SuppressWarnings("deprecation")
@@ -111,10 +145,19 @@ public class TicketCreationRunner implements Runnable{
 
 		String uriTicket = new StringBuilder("ServiceRequestCollection").toString();
 
-		BatchChangeSetPart changeRequestTicket = BatchChangeSetPart
-				.method("POST").uri(uriTicket)
-				.body(serializeTicketDeepInsert(ticket))
-				.headers(changeSetHeaders).contentId(contentId).build();
+		BatchChangeSetPart changeRequestTicket = null;
+		try {
+			changeRequestTicket = BatchChangeSetPart
+					.method("POST").uri(uriTicket)
+					.body(serializeTicketDeepInsert(ticket))
+					.headers(changeSetHeaders).contentId(contentId).build();
+		} catch (EntityProviderException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 		changeSet.add(changeRequestTicket);
 
@@ -123,13 +166,26 @@ public class TicketCreationRunner implements Runnable{
 		InputStream body = EntityProvider.writeBatchRequest(batchParts,
 				boundary);
 
-		String payload = IOUtils.toString(body);
+		String payload = null;
+		try {
+			payload = IOUtils.toString(body);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 
 		System.out.println("$batch request : ");
 
 		String serviceUrl = SERVICEURL;
 
-		HttpResponse batchResponse = executeBatchCall(serviceUrl, payload);
+		HttpResponse batchResponse = null;
+		try {
+			batchResponse = executeBatchCall(serviceUrl, payload);
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 		System.out.println("$batch response getStatusCode => "
 				+ batchResponse.getStatusLine().getStatusCode());
@@ -138,16 +194,37 @@ public class TicketCreationRunner implements Runnable{
 			System.out.println(h.getName() + ":" + h.getValue());
 		}
 
-		InputStream responseBody = batchResponse.getEntity().getContent();
+		InputStream responseBody = null;
+		try {
+			responseBody = batchResponse.getEntity().getContent();
+		} catch (UnsupportedOperationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		String contentType = batchResponse.getFirstHeader(
 				HttpHeaders.CONTENT_TYPE).getValue();
 		
-		String response = IOUtils.toString(responseBody);
+		String response = null;
+		try {
+			response = IOUtils.toString(responseBody);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		// Process the batch response to get the ticket key
-		List<BatchSingleResponse> responses = EntityProvider
-				.parseBatchResponse(IOUtils.toInputStream(response),
-						contentType);
+		List<BatchSingleResponse> responses = null;
+		try {
+			responses = EntityProvider
+					.parseBatchResponse(IOUtils.toInputStream(response),
+							contentType);
+		} catch (BatchException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		for (BatchSingleResponse rsp : responses) {
 			// Look for only created entries
 			System.out.println("Single Response status code => " + rsp.getStatusCode());
@@ -166,7 +243,23 @@ public class TicketCreationRunner implements Runnable{
 	
     @Override
 	public void run(){
-		System.out.println("Token: " + this.getXSRFToken());
+    	Ticket ticket = new Ticket();
+        ticket.setIssueCategory("CA_199");
+        ticket.setProductId("P400101");
+        ticket.setName("Testing ticket creation via OData");
+        ticket.setIssuePriority(IssuePriority.HIGH);
+        
+        /*// Add an incident description
+        Note incDesc = new Note();
+        incDesc.setNoteType(NoteType.INC_DESC);
+        incDesc.setNoteDescription("Incident description for ticket creation via OData");
+        
+        List<Note> incDescList = new ArrayList<Note>();
+        incDescList.add(incDesc);
+        
+        newTicket.setNotes(incDescList); */
+        
+    	this.createTicket(ticket);
 	}    	
 }
 
--- src/odata/model/Ticket.java
+++ src/odata/model/Ticket.java
@@ -1,5 +1,139 @@
 package odata.model;
 
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class Ticket {
+
+	private java.lang.String _ticketId;
+
+	private java.lang.String _ticketUUID;
+
+	private java.lang.String _name;
+
+	private Status _status;
+
+	private java.lang.String _customerNote;
+
+	private IssuePriority _issuePriority;
+
+	private String _issueCategory;
+
+	// private List<Note> _notes;
+
+	private String _productId;
+
+	private java.util.Date _createdAt;
+
+	private java.util.Date _changedAt;
+
+	public java.lang.String getTicketId() {
+		return _ticketId;
+	}
+
+	public java.lang.String getTicketUUID() {
+		return _ticketUUID;
+	}
+
+	public java.lang.String getName() {
+		return _name;
+	}
+
+	public Status getStatus() {
+		return _status;
+	}
+
+	public java.lang.String getCustomerNote() {
+		return _customerNote;
+	}
+
+	public IssuePriority getIssuePriority() {
+		return _issuePriority;
+	}
+
+	public String getIssueCategory() {
+		return _issueCategory;
+	}
+
+	/*public List<Note> getNotes() {
+		return _notes;
+	}*/
+
+	public String getProductId() {
+		return _productId;
+	}
+
+	public java.util.Date getCreatedAt() {
+		return _createdAt;
+	}
+
+	public java.util.Date getChangedAt() {
+		return _changedAt;
+	}
+
+	public void setTicketId(final java.lang.String _ticketId) {
+		this._ticketId = _ticketId;
+	}
+
+	public void setTicketUUID(final java.lang.String _ticketUUID) {
+		this._ticketUUID = _ticketUUID;
+	}
+
+	public void setName(final java.lang.String _name) {
+		this._name = _name;
+	}
+
+	public void setStatus(final Status _status) {
+		this._status = _status;
+	}
+
+	public void setCustomerNote(final java.lang.String _customerNote) {
+		this._customerNote = _customerNote;
+	}
+
+	public void setIssuePriority(final IssuePriority _issuePriority) {
+		this._issuePriority = _issuePriority;
+	}
+
+	public void setIssueCategory(final String _issueCategory) {
+		this._issueCategory = _issueCategory;
+	}
+
+	/*public void setNotes(final List<Note> _notes) {
+		this._notes = _notes;
+	}*/
+
+	public void setProductId(final String _productId) {
+		this._productId = _productId;
+	}
+
+	public void setCreatedAt(final java.util.Date _createdAt) {
+		this._createdAt = _createdAt;
+	}
+
+	public void setChangedAt(final java.util.Date _changedAt) {
+		this._changedAt = _changedAt;
+	}
+
+	@Override
+	public String toString() {
+
+		StringBuilder sb = new StringBuilder();
+		/*if (_notes != null) {
+			for (Note n : _notes) {
+				sb.append(n.toString());
+			}
+		}*/
+
+		return "Ticket [_ticketId=" + _ticketId + ", _ticketUUID="
+				+ _ticketUUID + ", _name=" + _name + ", _status="
+				+ _status + ", _customerNote=" + _customerNote
+				+ ", _issuePriority=" + _issuePriority + ", _issueCategory="
+				+ _issueCategory + ", _notes=" + sb.toString()
+				+ ", _productId=" + _productId + ", _createdAt=" + _createdAt
+				+ ", _changedAt=" + _changedAt + "]";
+	}
 
 }
