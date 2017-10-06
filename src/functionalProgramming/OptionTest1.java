14843278f24b71f8a22aaad006ab7f856e80d02f
cle.ee@163.com <cle.ee@163.com>
2017-10-06 22:45:59 +0800

    add two solutions

---
+++ src/functionalProgramming/OptionTest1.java
@@ -0,0 +1,28 @@
+package functionalProgramming;
+
+import java.util.Optional;
+
+public class OptionTest1 {
+
+	public Optional<Double> divideFirstTwo(JerryNumberProvider numberProvider, JerryMath math) {
+		    Optional<Integer> first = numberProvider.getNumber();
+		    Optional<Integer> second = numberProvider.getNumber();
+		    if(first.isPresent() && second.isPresent()) {
+		        double result = math.divide(first.get(), second.get());
+		        return Optional.of(result);
+		    } else {
+		        return Optional.empty();
+		    }
+	}
+	
+	public Optional<Double> divideFirstTwo2(JerryNumberProvider numberProvider, JerryMath math) {
+	    return numberProvider.getNumber().flatMap(first -> numberProvider.getNumber()
+	                                     .map(second -> math.divide(first, second)));
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
