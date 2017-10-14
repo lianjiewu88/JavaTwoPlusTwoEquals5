9d3c16d8da112871d7ee0d58586833e3c275c87d
cle.ee@163.com <cle.ee@163.com>
2017-10-14 11:29:23 +0800

    fix package error

--- src/wikitool/Launcher.java
+++ src/wikitool/Launcher.java
@@ -1,4 +1,4 @@
-
+package wikitool;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Vector;
 
 
 class wikiItem implements Comparable<wikiItem>
@@ -123,7 +122,6 @@ public class Launcher
 		Launcher tool = new Launcher("C:\\temp\\1.txt");
 		tool.run();
 	}
-
 }
 	
 	
