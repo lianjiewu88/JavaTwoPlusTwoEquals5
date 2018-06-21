ad4f65ee47a3b8eae978f0dd14f0358120b6d42f
cle.ee@163.com <cle.ee@163.com>
2018-06-21 11:40:51 +0800

    ready to read image content from ABAP

--- src/jco/StepByStepClient.java
+++ src/jco/StepByStepClient.java
@@ -24,6 +24,13 @@ public class StepByStepClient
 	static public final String PRODUCT_ID = "productID";
 	static public final String PRODUCT_TEXT = "productText";
 	
+	static private final String FILE_ID = "fileID";
+	static private final String FILE_OWNER = "fileOwner";
+	static private final String FILE_CDATE = "fileCreationDate";
+	static private final String FILE_NAME = "fileName";
+	static private final String FILE_CONTENT = "fileContent";
+	static private final String PRODUCT_IMAGES = "ProductImages";
+	
     static private Properties prepareProperty(){
         Properties connectProperties = new Properties();
         connectProperties.setProperty(DestinationDataProvider.JCO_ASHOST, "ldcixcd.wdf.sap.corp");
@@ -39,9 +46,61 @@ public class StepByStepClient
         return connectProperties;
     }
     
-    static public void main(String[] arg) {
-    	createDestinationDataFile(DESTINATION_NAME, prepareProperty());
-    	
+    static private void getProductImageTest(){
+    	JCoDestination destination = null;
+		try {
+			destination = JCoDestinationManager.getDestination(DESTINATION_NAME);
+			JCoRepository repo = destination.getRepository();
+    		JCoFunction stfcConnection = repo.getFunction("ZDIS_GET_MATERIAL_IMAGES");
+
+    		JCoParameterList imports = stfcConnection.getImportParameterList();
+        
+    		String materialID = "16";
+
+    		imports.setValue("IV_MATERIAL_ID", materialID);
+
+    		stfcConnection.execute(destination);
+        
+    		JCoParameterList exports = stfcConnection.getExportParameterList();
+    		
+    	    int abapDuration = exports.getInt("EV_DURATION");
+    	    
+    	    StringBuilder sb = new StringBuilder();
+    	    sb.append("{ \"" + ABAP_DURATION + "\": " + abapDuration + ",");
+    	    
+    	    sb.append("\"" + UPSELL_PRODUCT + "\":[");
+    	    
+    	    JCoTable codes = exports.getTable("ET_IMAGES");
+    	    
+    	    int row = codes.getNumRows();
+    	    System.out.println("Total rows: " + row);
+    	    
+    	    System.out.println("ABAP duration: " + abapDuration);
+    	    
+    	    for( int i = 0; i < row; i++){
+    	    	codes.setRow(i);
+                sb.append("{\"" + FILE_ID + "\":" + codes.getString("FILEID") + ","
+                		+ "\"" + FILE_OWNER + "\":\"" + codes.getString("OWNER") + "\"" + ",");
+                sb.append("{\"" + FILE_CDATE + "\":" + codes.getString("CREATION_DATE") + ","
+                		+ "\"" + FILE_NAME + "\":\"" + codes.getString("FILENAME") + "\""); 
+                if( i < row - 1){
+                	sb.append("},");
+                }
+                else{
+                	sb.append("}");
+                }
+    	    }
+    	    sb.append("]}");
+    	    
+    	    System.out.println("Final json: " + sb.toString());
+    	    
+		} catch (JCoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    static private void getUpsellProductTest(){
     	JCoDestination destination = null;
 		try {
 			destination = JCoDestinationManager.getDestination(DESTINATION_NAME);
@@ -95,7 +154,11 @@ public class StepByStepClient
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
+    }
+    
+    static public void main(String[] arg) {
+    	
+    	getProductImageTest();
     }
     
     static private void createDestinationDataFile(String destinationName, Properties connectProperties)
