5a893adc476e7f3576097855d7db02f4ec1718c9
cle.ee@163.com <cle.ee@163.com>
2018-10-02 12:44:03 +0800

    metadata works

--- src/image/pngTest.java
+++ src/image/pngTest.java
@@ -4,18 +4,18 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReader;
 import javax.imageio.metadata.IIOMetadata;
-
+import javax.imageio.metadata.IIOMetadataNode;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-
 import com.sun.imageio.plugins.png.PNGMetadata;
+
 public class pngTest {
 	
-	static public byte[] getContent(String filePath) throws IOException {  
+	static private byte[] getContent(String filePath) throws IOException {  
         File file = new File(filePath);  
         long fileSize = file.length();  
         if (fileSize > Integer.MAX_VALUE) {  
@@ -40,30 +40,53 @@ public class pngTest {
         return buffer;  
     }  
 	
-	static public String readCustomData(byte[] imageData, String key) throws IOException{
+	static private void readCustomData(byte[] imageData) throws IOException{
 	    ImageReader imageReader = ImageIO.getImageReadersByFormatName("png").next();
 
 	    imageReader.setInput(ImageIO.createImageInputStream(new ByteArrayInputStream(imageData)), true);
 
 	    IIOMetadata metadata = imageReader.getImageMetadata(0);
 
-	    //this cast helps getting the contents
 	    PNGMetadata pngmeta = (PNGMetadata) metadata; 
-	    NodeList childNodes = pngmeta.getStandardTextNode().getChildNodes();
-
-	    for (int i = 0; i < childNodes.getLength(); i++) {
+	    
+	    printNode(pngmeta.getStandardChromaNode());
+	    
+	    printNode(pngmeta.getStandardCompressionNode());
+	    
+	    printNode(pngmeta.getStandardDataNode());
+	    
+	    printNode(pngmeta.getStandardDimensionNode());
+	    
+	    printNode(pngmeta.getStandardDocumentNode());
+	    
+	    printNode(pngmeta.getStandardTextNode());
+	    
+	    printNode(pngmeta.getStandardTransparencyNode());
+	}
+	
+	static private void printNode(IIOMetadataNode metanode){
+		if (metanode == null)
+			return;
+		NodeList childNodes = metanode.getChildNodes();
+		if( childNodes == null)
+			return;
+		for (int i = 0; i < childNodes.getLength(); i++) {
 	        Node node = childNodes.item(i);
-	        String keyword = node.getAttributes().getNamedItem("keyword").getNodeValue();
-	        String value = node.getAttributes().getNamedItem("value").getNodeValue();
-	        if(key.equals(keyword)){
-	            return value;
+	        NamedNodeMap attribute = node.getAttributes();
+	        if( attribute == null)
+	        	continue;
+	        int length = attribute.getLength();
+	        for( int j = 0; j < length; j++){
+	        	Node each = attribute.item(j);
+	        	String value = each.getNodeValue();
+	        	String name = each.getNodeName();
+		        System.out.println("Name: " + name + " value: " + value);
 	        }
 	    }
-	    return null;
 	}
 	
 	static public void main(String[] arg) throws IOException{
 		byte[] content = getContent("C:\\Users\\i042416\\Desktop\\test\\clipboard1.png");
-		readCustomData(content, "dummy");
+		readCustomData(content);
 	}
 }
