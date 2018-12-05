49988846bec8552c6231359a087401dc38ae899a
cle.ee@163.com <cle.ee@163.com>
2018-12-05 15:13:41 +0800

    get json

--- src/youdao/YoudaoNote.java
+++ src/youdao/YoudaoNote.java
@@ -1,10 +1,31 @@
 package youdao;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
 
 public class YoudaoNote {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		HttpClient client = HttpClients.createDefault();
+	     
+	    String urlStr = "http://note.youdao.com/yws/public/note/7ef850508e67ea9cf586094766959f32?keyfrom=public";
+	        HttpGet get = new HttpGet(urlStr);
+	        try {
+	             
+	            HttpResponse response = client.execute(get);
+	            HttpEntity entity = response.getEntity();
+	            String result = EntityUtils.toString(entity, "UTF-8");
+	            System.out.print(result);
+	             
+	        } catch (Exception e){
+	        	e.printStackTrace();
+	        }
 
 	}
 
