cbaaa74c458b33cc634efbd5b09f0389cf1ea5a7
cle.ee@163.com <cle.ee@163.com>
2018-12-05 15:08:22 +0800

    recursive

--- src/recursive/ArrayMax.java
+++ src/recursive/ArrayMax.java
@@ -2,6 +2,7 @@ package recursive;
 
 public class ArrayMax {
 	public static void main(String[] rags) {
+		recur(0);
 		// int[] aim = new int[100];
 		int[] aim = { 1,4,3,10,34,1,4 };
 		int point = 0;
@@ -11,6 +12,11 @@ public class ArrayMax {
 		System.out.println("Max: " + max);
 	}
 
+	private static void recur(int i){
+		System.out.println("Stack level: " + i);
+		recur(++i);
+	}
+	
 	public static int getMax(int max, int point, int[] aim) {
 		if (point == aim.length)
 			return max;
